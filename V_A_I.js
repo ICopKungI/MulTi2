3d5e042988a1c4b436e32aed6943ea1755f7a2b5
PanupongSoongwiriya <61070345@kmitl.ac.th>
2019-05-24 01:19:01 +0700

    test

--- V_A_I.js
+++ V_A_I.js
@@ -1,14 +1,14 @@
 window.location.assign("#popup0")
 function countDown(){
     console.log(seconds)
-    seconds--
-    if (seconds == 0){
+    if (seconds <= 0){
         window.location.assign("#")
         clearInterval(i);
     }
+    seconds--
 }
 //กำหนดเวลา วินาทีที่จะให้ popup ทำงาน 
-var seconds = 1,
+var seconds = 21,
 i = setInterval(countDown, 1000);
 var myIndex_0 = 0;
 var myIndex_1 = 0;
@@ -39,7 +39,7 @@ function carousel() {
 	   	x_0[i_0].style.display = "none";  
 	}
 	myIndex_0++;
-	if (myIndex_0 > x_0.length) {myIndex_0 = 1}    
+	if (myIndex_0 > x_0.length) {myIndex_0 = 1}
 		x_0[myIndex_0-1].style.display = "block";  
 	var i_1;
 	var x_1 = document.getElementsByClassName("mySlides1");
@@ -47,7 +47,7 @@ function carousel() {
 	   	x_1[i_1].style.display = "none";  
 	}
 	myIndex_1++;
-	if (myIndex_1 > x_1.length) {myIndex_1 = 1}    
+	if (myIndex_1 > x_1.length) {myIndex_1 = 1}
 		x_1[myIndex_1-1].style.display = "block";
 	var i_2;
 	var x_2 = document.getElementsByClassName("mySlides2");
@@ -55,7 +55,7 @@ function carousel() {
 	   	x_2[i_2].style.display = "none";  
 	}
 	myIndex_2++;
-	if (myIndex_2 > x_2.length) {myIndex_2 = 1}    
+	if (myIndex_2 > x_2.length) {myIndex_2 = 1}
 		x_2[myIndex_2-1].style.display = "block";
 	var i_3;
 	var x_3 = document.getElementsByClassName("mySlides3");
@@ -63,15 +63,15 @@ function carousel() {
 	   	x_3[i_3].style.display = "none";  
 	}
 	myIndex_3++;
-	if (myIndex_3 > x_3.length) {myIndex_3 = 1}    
+	if (myIndex_3 > x_3.length) {myIndex_3 = 1}
 		x_3[myIndex_3-1].style.display = "block";
 	var i_4;
-	var x_4 = document.getElementsByClassName("mySlides4");
+	/*var x_4 = document.getElementsByClassName("mySlides4");
 	for (i_4 = 0; i_4 < x_4.length; i_4++) {
 	   	x_4[i_4].style.display = "none";  
 	}
 	myIndex_4++;
-	if (myIndex_4 > x_4.length) {myIndex_4 = 1}    
+	if (myIndex_4 > x_4.length) {myIndex_4 = 1}
 		x_4[myIndex_4-1].style.display = "block";
 	var i_5;
 	var x_5 = document.getElementsByClassName("mySlides5");
@@ -79,7 +79,7 @@ function carousel() {
 	   	x_5[i_5].style.display = "none";  
 	}
 	myIndex_5++;
-	if (myIndex_5 > x_5.length) {myIndex_5 = 1}    
+	if (myIndex_5 > x_5.length) {myIndex_5 = 1}
 		x_5[myIndex_5-1].style.display = "block";
 	var i_6;
 	var x_6 = document.getElementsByClassName("mySlides6");
@@ -87,7 +87,7 @@ function carousel() {
 	   	x_6[i_6].style.display = "none";  
 	}
 	myIndex_6++;
-	if (myIndex_6 > x_6.length) {myIndex_6 = 1}    
+	if (myIndex_6 > x_6.length) {myIndex_6 = 1}
 		x_6[myIndex_6-1].style.display = "block";
 	var i_7;
 	var x_7 = document.getElementsByClassName("mySlides7");
@@ -95,7 +95,7 @@ function carousel() {
 	   	x_7[i_7].style.display = "none";  
 	}
 	myIndex_7++;
-	if (myIndex_7 > x_7.length) {myIndex_7 = 1}    
+	if (myIndex_7 > x_7.length) {myIndex_7 = 1}
 		x_7[myIndex_7-1].style.display = "block";
 	var i_8;
 	var x_8 = document.getElementsByClassName("mySlides8");
@@ -103,7 +103,7 @@ function carousel() {
 	   	x_8[i_8].style.display = "none";  
 	}
 	myIndex_8++;
-	if (myIndex_8 > x_8.length) {myIndex_8 = 1}    
+	if (myIndex_8 > x_8.length) {myIndex_8 = 1}
 		x_8[myIndex_8-1].style.display = "block";
 	var i_9;
 	var x_9 = document.getElementsByClassName("mySlides9");
@@ -111,7 +111,7 @@ function carousel() {
 	   	x_9[i_9].style.display = "none";  
 	}
 	myIndex_9++;
-	if (myIndex_9 > x_9.length) {myIndex_9 = 1}    
+	if (myIndex_9 > x_9.length) {myIndex_9 = 1}
 		x_9[myIndex_9-1].style.display = "block";
 	var i_10;
 	var x_10 = document.getElementsByClassName("mySlides10");
@@ -119,7 +119,7 @@ function carousel() {
 	   	x_10[i_10].style.display = "none";  
 	}
 	myIndex_10++;
-	if (myIndex_10 > x_10.length) {myIndex_10 = 1}    
+	if (myIndex_10 > x_10.length) {myIndex_10 = 1}
 		x_10[myIndex_10-1].style.display = "block";
 	var i_11;
 	var x_11 = document.getElementsByClassName("mySlides11");
@@ -127,7 +127,7 @@ function carousel() {
 	   	x_11[i_11].style.display = "none";  
 	}
 	myIndex_11++;
-	if (myIndex_11 > x_11.length) {myIndex_11 = 1}    
+	if (myIndex_11 > x_11.length) {myIndex_11 = 1}
 		x_11[myIndex_11-1].style.display = "block";
 	var i_12;
 	var x_12 = document.getElementsByClassName("mySlides12");
@@ -135,7 +135,7 @@ function carousel() {
 	   	x_12[i_12].style.display = "none";  
 	}
 	myIndex_12++;
-	if (myIndex_12 > x_12.length) {myIndex_12 = 1}    
+	if (myIndex_12 > x_12.length) {myIndex_12 = 1}
 		x_12[myIndex_12-1].style.display = "block";
 	var i_13;
 	var x_13 = document.getElementsByClassName("mySlides13");
@@ -143,7 +143,7 @@ function carousel() {
 	   	x_13[i_13].style.display = "none";  
 	}
 	myIndex_13++;
-	if (myIndex_13 > x_13.length) {myIndex_13 = 1}    
+	if (myIndex_13 > x_13.length) {myIndex_13 = 1}
 		x_13[myIndex_13-1].style.display = "block";
 	var i_14;
 	var x_14 = document.getElementsByClassName("mySlides14");
@@ -151,7 +151,7 @@ function carousel() {
 	   	x_14[i_14].style.display = "none";  
 	}
 	myIndex_14++;
-	if (myIndex_14 > x_14.length) {myIndex_14 = 1}    
+	if (myIndex_14 > x_14.length) {myIndex_14 = 1}
 		x_14[myIndex_14-1].style.display = "block";
 	var i_15;
 	var x_15 = document.getElementsByClassName("mySlides15");
@@ -159,7 +159,7 @@ function carousel() {
 	   	x_15[i_15].style.display = "none";  
 	}
 	myIndex_15++;
-	if (myIndex_15 > x_15.length) {myIndex_15 = 1}    
+	if (myIndex_15 > x_15.length) {myIndex_15 = 1}
 		x_15[myIndex_15-1].style.display = "block";
 	var i_16;
 	var x_16 = document.getElementsByClassName("mySlides16");
@@ -167,7 +167,7 @@ function carousel() {
 	   	x_16[i_16].style.display = "none";  
 	}
 	myIndex_16++;
-	if (myIndex_16 > x_16.length) {myIndex_16 = 1}    
+	if (myIndex_16 > x_16.length) {myIndex_16 = 1}
 		x_16[myIndex_16-1].style.display = "block";
 	var i_17;
 	var x_17 = document.getElementsByClassName("mySlides17");
@@ -175,7 +175,7 @@ function carousel() {
 	   	x_17[i_17].style.display = "none";  
 	}
 	myIndex_17++;
-	if (myIndex_17 > x_17.length) {myIndex_17 = 1}    
+	if (myIndex_17 > x_17.length) {myIndex_17 = 1}
 		x_17[myIndex_17-1].style.display = "block";
 	var i_18;
 	var x_18 = document.getElementsByClassName("mySlides18");
@@ -183,7 +183,7 @@ function carousel() {
 	   	x_18[i_18].style.display = "none";  
 	}
 	myIndex_18++;
-	if (myIndex_18 > x_18.length) {myIndex_18 = 1}    
+	if (myIndex_18 > x_18.length) {myIndex_18 = 1}
 		x_18[myIndex_18-1].style.display = "block";
 	var i_19;
 	var x_19 = document.getElementsByClassName("mySlides19");
@@ -191,7 +191,7 @@ function carousel() {
 	   	x_19[i_19].style.display = "none";  
 	}
 	myIndex_19++;
-	if (myIndex_19 > x_19.length) {myIndex_19 = 1}    
-		x_19[myIndex_19-1].style.display = "block";
+	if (myIndex_19 > x_19.length) {myIndex_19 = 1}
+		x_19[myIndex_19-1].style.display = "block";*/
 	setTimeout(carousel, 2000); // Change image every 2 seconds
 }
